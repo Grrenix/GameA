e5d96b5ee5f8fc5d4f8976d89491f388aff0418a
Grrenix <grrenix@gmail.com>
2026-02-06 10:21:00 +0000

    Remove WinMain

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 
-#ifdef WIN32
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-#else
+#include <engine/application/application.hpp>
+
 int main(int argc, char *argv[])
-#endif
 {
     std::cout << "Testing" << '\n';
 }
