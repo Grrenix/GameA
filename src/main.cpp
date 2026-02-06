10d0b9ae49b9ec79533d5545617ce101a59d8a3b
Grrenix <grrenix@gmail.com>
2026-02-06 09:22:21 +0000

    Get Project setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int argc, char *argv[])
+#endif
+{
+    std::cout << "Testing" << '\n';
+}
