1a3ee8c919fa3bd81f138e10a0b02a9507d60b66
Grrenix <grrenix@gmail.com>
2026-02-06 14:50:05 +0000

    Add subscribe for WindowOpenedEvent

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <engine/prelude.hpp>
+#include <engine/events/window_events.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -8,8 +9,13 @@ int main(int argc, char *argv[])
     windowSpec.Title = "Game A";
     windowSpec.Width = 1920;
     windowSpec.Height = 1080;
+    windowSpec.Fullscreen = false;
+    windowSpec.Borderless = false;
 
     engine::Application *app = engine::Application::New()->WithWindowSpec(&windowSpec);
+
+    app->m_EventDispatcher->Subscribe<engine::WindowOpenedEvent>([](const engine::WindowOpenedEvent &e)
+                                                                 { std::cout << "Window Opened! " << e.Data << '\n'; });
 
     app->Run();
 }
