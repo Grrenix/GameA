04467b56448122dc60d94c5ce6e7940c8cc2851d
Grrenix <grrenix@gmail.com>
2026-02-06 11:29:22 +0000

    Create basic window

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Testing" << '\n';
+    engine::WindowSpec windowSpec = engine::WindowSpec();
+    windowSpec.Title = "Game A";
+    windowSpec.Width = 1920;
+    windowSpec.Height = 1080;
+
+    engine::Application *app = engine::Application::New()->WithWindowSpec(&windowSpec);
+
+    app->Run();
 }
