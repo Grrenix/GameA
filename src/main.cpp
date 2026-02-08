d45c4efd8f487d918cd0d99a630013a297218ede
Grrenix <grrenix@gmail.com>
2026-02-08 02:05:20 +0000

    Update engine and add mouse event handling

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,6 @@ int main(int argc, char *argv[])
     app->m_EventDispatcher->Subscribe<engine::WindowOpenedEvent>(
         [](const engine::WindowOpenedEvent &e)
         { std::cout << "Window Opened!" << '\n'; });
-
     app->m_EventDispatcher->Subscribe<engine::WindowDestroyedEvent>(
         [](const engine::WindowDestroyedEvent &e)
         { std::cout << "Window Destroyed!" << '\n'; });
@@ -27,10 +26,23 @@ int main(int argc, char *argv[])
     app->m_EventDispatcher->Subscribe<engine::KeyPressedEvent>(
         [](const engine::KeyPressedEvent &e)
         { std::cout << "Key Pressed! " << engine::input::KeyToString(e.Data) << '\n'; });
-
     app->m_EventDispatcher->Subscribe<engine::KeyReleasedEvent>(
         [](const engine::KeyReleasedEvent &e)
         { std::cout << "Key Released! " << engine::input::KeyToString(e.Data) << '\n'; });
+
+    app->m_EventDispatcher->Subscribe<engine::MouseMovedEvent>(
+        [](const engine::MouseMovedEvent &e)
+        { std::cout << "Mouse Moved! (" << e.PosX << ", " << e.PosY << ")" << '\n'; });
+    app->m_EventDispatcher->Subscribe<engine::MouseScrollEvent>(
+        [](const engine::MouseScrollEvent &e)
+        { std::cout << "Mouse Scrolled! (" << e.DirX << ", " << e.DirY << ")" << '\n'; });
+
+    app->m_EventDispatcher->Subscribe<engine::MouseButtonClickEvent>(
+        [](const engine::MouseButtonClickEvent &e)
+        { std::cout << "Mouse button clicked! (" << engine::input::MouseButtonToString(e.Button) << ")" << '\n'; });
+    app->m_EventDispatcher->Subscribe<engine::MouseButtonReleaseEvent>(
+        [](const engine::MouseButtonReleaseEvent &e)
+        { std::cout << "Mouse button released! (" << engine::input::MouseButtonToString(e.Button) << ")" << '\n'; });
 
     app->m_EventDispatcher->Subscribe<engine::UpdateEvent>(
         [&](const engine::UpdateEvent &e)
